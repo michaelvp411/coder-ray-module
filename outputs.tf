10bd5538c598b206225fc264095558ea731b164a
Michael Vincent Patterson <michael@MacBook-Pro.local>
2025-09-18 11:04:01 -0400

    simplified outputs

--- outputs.tf
+++ outputs.tf
@@ -8,18 +8,7 @@ output "ray_dashboard_app_id" {
   value       = coder_app.ray-dashboard.id
 }
 
-output "ray_venv_path" {
-  description = "Path to the Ray Python virtual environment"
-  value       = var.venv_path
-}
-
 output "ray_port" {
   description = "The port number used by Ray Dashboard"
   value       = var.port
 }
-
-output "ray_setup_script_id" {
-  description = "The ID of the Ray setup script resource (if auto_install_ray is enabled)"
-  value       = var.auto_install_ray ? coder_script.ray_setup[0].id : null
-}
-
